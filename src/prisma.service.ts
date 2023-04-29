f4ca19c37629830fee916a4773f0b8ac8666d921
hwennnn <hwendev@gmail.com>
2023-04-29 16:44:34 +0800

    feat(prisma): implement prisma service

---
+++ src/prisma.service.ts
@@ -0,0 +1,19 @@
+import {
+  Injectable,
+  type INestApplication,
+  type OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication): Promise<void> {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
