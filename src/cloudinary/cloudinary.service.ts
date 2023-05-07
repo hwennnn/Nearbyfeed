b079ce8276748e02e537010b690cf4ae8b6f3aec
hwennnn <hwendev@gmail.com>
2023-05-07 18:05:09 +0800

    feat(cloudinary): restrict images size to 400x400

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -14,18 +14,25 @@ export class CloudinaryService {
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return await new Promise((resolve, reject) => {
-      const stream = v2.uploader.upload_stream((error, result) => {
-        if (error !== undefined || result === undefined) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
+      const upload = v2.uploader.upload_stream(
+        {
+          width: 400,
+          height: 400,
+          crop: 'limit',
+        },
+        (error, result) => {
+          if (error !== undefined || result === undefined) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        },
+      );
 
       const readableStream = new Readable();
       readableStream.push(file.buffer);
       readableStream.push(null);
-      readableStream.pipe(stream);
+      readableStream.pipe(upload);
     });
   }
 }
